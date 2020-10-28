ffe22b60f0a4b6109bcb0b1e68f8fda2c6a7cd71
obbijuan <juan.garces@outlook.com>
2020-10-28 00:45:18 -0300

    __str__ added

--- poo/class/Card.py
+++ poo/class/Card.py
@@ -1,12 +1,12 @@
 from typing import Tuple
 from enum import Enum
 
+
 class Suit(str, Enum):
     Club = "♣"
     Diamond = "♦"
     Heart = "♥"
     Spade = "♠"
-    Test = "°"
 
 
 class Card:
@@ -15,6 +15,9 @@ class Card:
         self.suit = suit
         self.rank = rank
         self.hard, self.soft = self._points()
+
+    def __str__(self):
+        return str(self.__class__.__name__) + ": " + str(self.__dict__)
 
     def _points(self) -> Tuple[int, int]:
         return int(self.rank), int(self.rank)
@@ -25,6 +28,27 @@ class AceCard(Card):
         return 1, 11
 
 class FaceCard(Card):
-    
+
     def _points(self) -> Tuple[int, int]:
         return 10, 10
+
+
+def card(rank: int, suit: Suit) -> Card:
+    if rank == 1:
+        return AceCard("A", suit)
+    elif 2 <= rank < 11:
+        return Card(str(rank), suit)
+    elif 11 <= rank < 14:
+        name = {11: "J", 12: "Q", 13: "K"}[rank]
+        return FaceCard(name, suit)
+    raise Exception("Design Failure")
+
+
+cards = [AceCard('A', Suit.Spade), Card('2', Suit.Spade), FaceCard('Q', Suit.Spade),]
+for x in cards:             
+    print(x)
+
+cards2 = [ card(rank, suit) for rank in range(1,14) for suit in Suit.Diamond ]
+for x in cards2:             
+    print(x)
+
